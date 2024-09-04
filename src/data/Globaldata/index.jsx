88d7943f93ed45bb2b484bcfcbdc02fa4b4c4d16
J4Admin <159776479+J4Admin@users.noreply.github.com>
2024-09-04 14:25:50 +0200

    Update index.jsx

--- src/data/Globaldata/index.jsx
+++ src/data/Globaldata/index.jsx
@@ -1,5 +1,5 @@
 export const NAME = "Léo Haddou";
-export const URL_CURRICULUM = "Curriculum";
+export const URL_CURRICULUM = "curriculum";
 export const URL_GIT = "https://github.com/J4Admin";
 export const URL_LINKEDIN = "https://www.linkedin.com/in/leo-h-256122299/";
 export const EMAIL = "leo.haddou951@gmail.com";
