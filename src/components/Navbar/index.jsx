7a45593e6b865665e45b3231068246300ccabb44
J4Admin <159776479+J4Admin@users.noreply.github.com>
2024-09-10 16:05:14 +0200

    add: burgerMenu in navbar components, and add transition / logic for burgerMenu

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,27 +1,39 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
+
 export default function Navbar() {
+  const [showLinks, SetShowLinks] = useState(false);
+  const handleShowLinks = () => {
+    SetShowLinks(!showLinks);
+  };
+  const closeMenu = () => {
+    SetShowLinks(false);
+  };
+
   return (
     <>
       <nav id="navbar">
         <h2 className="navbar-title"> L.H </h2>{" "}
-        <ul id="navbar-list">
+        <ul id="navbar-list" className={`${showLinks ? "active" : "hidden"}`}>
           <li className="navbar-item">
-            <Link className="navbar-link" to="/">
+            <Link className="navbar-link" to="/" onClick={closeMenu}>
               Présentation
             </Link>
           </li>
           <li className="navbar-item">
-            <Link className="navbar-link" to="projets">
+            <Link className="navbar-link" to="projets"onClick={closeMenu}>
               Projets
             </Link>
           </li>
           <li className="navbar-item">
-            <Link className="navbar-link" to="contact">
+            <Link className="navbar-link" to="contact"onClick={closeMenu}>
               Contact
             </Link>
           </li>
+          <button className="navbar-burger" onClick={handleShowLinks}>
+            <span className="burger-bar"></span>
+          </button>
         </ul>
       </nav>
     </>
